e283984ac440985c5fe77b231e2ab9fb1a14d3ff
[Brian Kiehn] <[briankiehn@comcast.net]>
2019-05-06 02:18:15 -0400

    made sticky but fails to validate or submit

--- index.php
+++ index.php
@@ -3,8 +3,8 @@
  * Dating site for gay frogs
  * Created by PhpStorm.
  * @author Brian Kiehn
- * @version 2.0
- * @date 4/12/2019
+ * @version 3.0
+ * @date 5/5/2019
  *
  */
 
@@ -35,45 +35,86 @@ $f3->route('GET /', function(){
 });
 
 //Form routes
-$f3->route('GET /form', function() {
+$f3->route('GET|POST /form', function($f3) {
     //display a view
-    $view = new Template();
+    $template = new Template();
 
-    echo $view->render('views/form1.html');
+    //check if $POST even exists, then validate
+    if (isset($_POST['fn'])&&isset($_POST['ln'])&&isset($_POST['age'])
+        &&isset($_POST['g'])&&isset($_POST['ph'])) {
+        //check valid strings and numbers
+        if (validString($_POST['fn'])&&validString($_POST['ln'])&&is_Numeric($_POST['age'])
+            &&is_numeric($_POST['ph'])) {
+
+            $_SESSION ['fn'] = $_POST['fn'];
+            $_SESSION ['ln'] = $_POST['ln'];
+            $_SESSION ['age'] = $_POST['age'];
+            $_SESSION ['g'] = $_POST['g'];
+            $_SESSION ['ph'] = $_POST['ph'];
+
+            $f3->reroute('/info');
+        }
+        else
+        {
+            //instantiate an error array with message
+            if(!validString($_POST['fn'])||validString($_POST['ln'])){
+                $f3->set("error: not a valid name.");
+            }
+            if((!is_Numeric($_POST['age']))){
+                $f3->set("error: not a valid age.");
+            }
+            if(!is_numeric($_POST['ph'])){
+                $f3->set("error: not a valid phone number.");
+            }
+        }
+    }
+    echo $template->render('views/form1.html');
 });
 
-$f3->route('GET|POST /info', function() {
+$f3->route('GET|POST /info', function($f3) {
+    $template = new Template();
+    //check if $POST even exists, then validate
+    if (isset($_POST['em'])&&isset($_POST['st'])&&isset($_POST['bio'])) {
+        //check valid strings and numbers
+        if (filter_var($_POST['em'], FILTER_VALIDATE_EMAIL)&&
+            validString($_POST['st'])) {
 
-    $_SESSION ['fn'] = $_POST['fn'];
-    $_SESSION ['ln'] = $_POST['ln'];
-    $_SESSION ['age'] = $_POST['age'];
-    $_SESSION ['g'] = $_POST['g'];
-    $_SESSION ['ph'] = $_POST['ph'];
+            $_SESSION ['em'] = $_POST['em'];
+            $_SESSION ['st'] = $_POST['st'];
+            $_SESSION ['bio'] = $_POST['bio'];
 
-
-    //display a view
-    $view = new Template();
-    echo $view->render('views/form2.html');
+            $f3->reroute('/hobbies');
+        }
+        else
+        {
+            //instantiate an error array with message
+            if(!(filter_var($_POST['em'], FILTER_VALIDATE_EMAIL))){
+                $f3->set("error: not a valid email.");
+            }
+            if(!validString($_POST['st'])){
+                $f3->set("error: not a valid state.");
+            }
+        }
+    }
+    echo $template->render('views/form2.html');
 });
 
 $f3->route('GET|POST /hobbies', function() {
-
-    $_SESSION ['em'] = $_POST['em'];
-    $_SESSION ['st'] = $_POST['st'];
-    $_SESSION ['bio'] = $_POST['bio'];
 
     //display a view
     $view = new Template();
     echo $view->render('views/form3.html');
 });
 
-$f3->route('GET|POST /profile', function($f3){
+$f3->route('GET|POST /profile', function(){
     //display a view
     $view = new Template();
-
-    $hb = $_POST['hobbies'];
-    $_SESSION ['hb'] = $hb;
-
+/*
+    $_SESSION ['hb'] = $_POST['hobbies'];
+    foreach($_SESSION['hb'] as $value){
+        echo $value.',';
+    };
+*/
     echo $view->render('views/profile.html');
 });
 
