253529ecede5c64927e9da0575b7a16b08f5765c
[Brian Kiehn] <[briankiehn@comcast.net]>
2019-05-09 02:52:18 -0400

    Added validation functions

--- model/validation-functions.php
+++ model/validation-functions.php
@@ -2,8 +2,8 @@
 /**
  * Created by PhpStorm.
  * @author Brian Kiehn
- * @version 1.0
- * @date 5/5/2019
+ * @version 2.0
+ * @date 5/8/2019
  *
  */
 
@@ -11,7 +11,26 @@
 /*
  * @param $String
  */
-function validString($String)
-{
+function validName($String){
     return ctype_alpha($String) AND ($String !="");
+}
+
+function validAge($age){
+    return is_Numeric($age) AND  (18 > $age AND  $age < 118);
+}
+
+function validPhone($phone){
+    return preg_match("/^[0-9][3}-[0-9]{4}-[0-9]{4}$/", $phone);
+}
+
+function validEmail($email){
+    return filter_var($_POST['em'], FILTER_VALIDATE_EMAIL);
+}
+
+function validOutdoor(){
+
+}
+
+function validIndoor(){
+
 }
