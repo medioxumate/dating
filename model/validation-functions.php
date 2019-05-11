aac0425c3c4794f8019f8000294448e605f4d278
[Brian Kiehn] <[briankiehn@comcast.net]>
2019-05-11 06:06:32 -0400

    added validation for the interests page

--- model/validation-functions.php
+++ model/validation-functions.php
@@ -7,7 +7,6 @@
  *
  */
 
-
 //functions
 /*
  * @param $String
@@ -17,14 +16,12 @@ function validName($String){
 }
 
 function validAge($age){
-    if(is_Numeric($age) AND 18 > $age AND  $age < 118) {
-        return true;
-    };
+    return is_Numeric($age) AND 18 < $age AND  $age < 118;
 }
 
 function validPhone($phone){
-    return preg_match("/^[0-9][3}-[0-9]{4}-[0-9]{4}$/", $phone)||
-        preg_match("/^[0-9][3}[0-9]{4}[0-9]{4}$/", $phone);
+    return preg_match("/^[0-9]{3}-[0-9]{3}-[0-9]{4}$/", $phone)||
+        preg_match("/^[0-9]{3}[0-9]{3}[0-9]{4}$/", $phone);
 }
 
 function validEmail($email){
@@ -32,13 +29,63 @@ function validEmail($email){
 }
 
 function validState($state){
+//Array of valid states
+    $validStates = array('Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California', 'Colorado',
+        'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii', 'Idaho', 'Illinois',
+        'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana', 'Maine', 'Maryland',
+        'Massachusetts', 'Michigan', 'Minnesota', 'Mississippi', 'Missouri', 'Montana',
+        'Nebraska', 'Nevada', 'New Hampshire', 'New Jersey', 'New Mexico', 'New York',
+        'North Carolina', 'North Dakota', 'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania',
+        'Rhode Island', 'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah',
+        'Vermont', 'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming',
+        'American Samoa', 'District of Columbia', 'Guam', 'Marshall Islands', 'Northern Mariana Islands',
+        'Palau', 'Puerto Rico', 'Virgin Islands');
 
+    $valid = false;
+    foreach ($validStates as $test){
+        if($test == $state){
+            $valid = true;
+        }
+    }
+
+    return $valid;
 }
 
-function validOutdoor(){
+function validIndoor(array $indoor){
+    $validIndoor = array('tv','puzzles', 'movies', 'reading', 'cooking',
+        'playing cards', 'board games', 'video games');
 
+    $valid = false;
+    $count = 0;
+    foreach ($validIndoor as $door){
+        foreach ($indoor as $in){
+            if($door == $in){
+                $count++;
+            }
+        }
+    }
+    if($count == sizeof($indoor)){
+        $valid = true;
+    }
+
+    return $valid;
 }
 
-function validIndoor(){
+function validOutdoor(array $outdoor){
+    $validOutdoor = array('swimming','hopping', 'floating', 'collecting', 'croaking');
 
+    $valid = false;
+    $count = 0;
+    foreach ($validOutdoor as $door){
+        foreach ($outdoor as $out){
+            if($door == $out){
+                $count++;
+            }
+        }
+    }
+    if($count == sizeof($outdoor)){
+        $valid = true;
+    }
+
+    return $valid;
 }
